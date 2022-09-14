b24470fa20759173fe8a95c8b2f073c39a7ec845
ChungyiBossi <chungyi.bossi@gmail.com>
2022-09-14 15:58:36 +0800

    [refactor]

--- cvzone_sample/face_depth_detector.py
+++ cvzone_sample/face_depth_detector.py
@@ -1,10 +1,10 @@
 import cv2
 import cvzone
+# from cvzone.FaceDetectionModule import FaceDetector()
 from cvzone.FaceMeshModule import FaceMeshDetector
 
-cap = cv2.VideoCapture(0)
 detector = FaceMeshDetector(maxFaces=1)
-
+cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
     img, faces = detector.findFaceMesh(img, draw=False)
@@ -14,25 +14,28 @@ while True:
         pointLeft = face[145]
         pointRight = face[374]
         # Drawing
-        # cv2.line(img, pointLeft, pointRight, (0, 200, 0), 3)
-        # cv2.circle(img, pointLeft, 5, (255, 0, 255), cv2.FILLED)
-        # cv2.circle(img, pointRight, 5, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, pointLeft, pointRight, (0, 200, 0), 3)
+        cv2.circle(img, pointLeft, 5, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, pointRight, 5, (255, 0, 255), cv2.FILLED)
         w, _ = detector.findDistance(pointLeft, pointRight)
         W = 6.3
 
         # # Finding the Focal Length
-        # d = 50 # 你的距離
+        # d = 50  # 你的距離
         # f = (w*d)/W
-        # print(f)
+        # print("Focus: ", f)
 
         # Finding distance
-        f = 840
+        f = 1500
         d = (W * f) / w
-        print(d)
+        print("Distance: ", d)
 
         cvzone.putTextRect(img, f'Depth: {int(d)}cm',
-                           (face[10][0] - 100, face[10][1] - 50),
+                           (face[10][0] - 100, face[10][1] - 50),  # 10 是頭頂
                            scale=2)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        cv2.destroyAllWindows()
+        break
--- cvzone_sample/hand_depth_detection.py
+++ cvzone_sample/hand_depth_detection.py
@@ -6,8 +6,8 @@ import cvzone
 
 # Webcam
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 # Hand Detector
 detector = HandDetector(detectionCon=0.8, maxHands=2)
@@ -32,6 +32,8 @@ while True:
         x1, y1 = lmList[5][:2]
         x2, y2 = lmList[17][:2]
 
+        # cv2.circle(img, (x1, y1), 30, [0, 0, 255], cv2.FILLED)
+        # cv2.circle(img, (x2, y2), 30, [0, 255, 0], cv2.FILLED)
         distance = int(math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2))
         A, B, C = coff
         distanceCM = A * distance ** 2 + B * distance + C
@@ -42,4 +44,7 @@ while True:
         cvzone.putTextRect(img, f'{int(distanceCM)} cm', (x, y+h+10))
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        cv2.destroyAllWindows()
+        break
